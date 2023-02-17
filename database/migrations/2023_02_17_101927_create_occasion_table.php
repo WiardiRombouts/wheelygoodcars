3ba6a8ad33630acc56fa63af39e37b3985313c48
WiardiRombouts <wiardi18@gmail.com>
2023-02-17 11:39:20 +0100

    occasion migration

---
+++ database/migrations/2023_02_17_101927_create_occasion_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('occasion', function (Blueprint $table) {
+            $table->id();
+            $table->string('license_plate');
+            $table->string('brand');
+            $table->string('model');
+            $table->string('fuel')->nullable();
+            $table->string('body')->nullable();
+            $table->integer('seats')->nullable();
+            $table->integer('doors')->nullable();
+            $table->decimal('mileage')->nullable();
+            $table->integer('production_year')->nullable();
+            $table->integer('horsepower')->nullable();
+            $table->string('transmission')->nullable();
+            $table->integer('cilinders')->nullable();
+            $table->integer('cilinder_capacity')->nullable();
+            $table->integer('weight')->nullable();
+            $table->string('color')->nullable();
+            $table->string('description')->nullable();
+            $table->decimal('price')->nullable();
+            $table->string('image')->nullable();
+            $table->tinyInteger('sold');
+            $table->integer('seller_id');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('occasion');
+    }
+};
