6f62f019868931e8b3414340d8c8a26e40b577de
Wiardi Rombouts <wiardi18@gmail.com>
2023-02-21 09:29:14 +0100

    new route to all cars

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+    public function show_all_cars_page(){
+        return view('cars');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +20,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware('auth')->group(function () {
-    //
+    Route::get('/cars/show', [CarController::class, 'show_all_cars_page'])->name('show_all_cars_page');
 });
 
 require __DIR__.'/auth.php';
