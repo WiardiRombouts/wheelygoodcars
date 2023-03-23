8af5314ed2cbaf29d885fc4e77d2b8362448ca54
Wiardi Rombouts <wiardi18@gmail.com>
2023-03-23 16:38:54 +0100

    Sale status implemented

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -3,24 +3,30 @@
     <table class="table car_overview table-hover">
 
         <tbody>
-
-
             @foreach ($cars as $car)
                 <tr class="table-striped">
                     <th class="table_item car_picture">
                         @if ($car->image == null)
-                            <img src="{{ URL::asset('/images/placeholder-small.jpg') }}" alt="profile Pic" height="85"
-                                width="100">
+                            <img src="{{ URL::asset('/images/placeholder-small.jpg') }}" alt="profile Pic" height="120"
+                                width="135">
                         @else
                             {{ $car->image }}
                         @endif
                     </th>
-                    <td>
-                        <div class="kenteken license_plate_in_list">
+                    <td class="table_item">
+                        <div class="kenteken license_plate_in_list d-flex flex-column align-items-center">
                             <div class="inset">
                                 <div class="blue"></div>
                                 <input type="text" name="license_plate" disabled="" value="{{ $car->license_plate }}"
                                     required="" />
+                            </div>
+                            <div class="sale_status">
+                                @isset($car->sold_at)
+                                    <p class="status_text border border-dark rounded text-light bg-danger">VERKOCHT</p>
+                                @endisset
+                                @empty($car->sold_at)
+                                    <p class="status_text border border-dark rounded text-light bg-success">TE KOOP</p>
+                                @endempty
                             </div>
                         </div>
                     </td>
--- resources/views/personal_cars.blade.php
+++ resources/views/personal_cars.blade.php
@@ -1,42 +1,58 @@
 @extends('layouts.app')
 @section('main')
-<table class="table personal_car_overview table-hover">
-    
-    <tbody>
-        @foreach ($cars as $car)
+    <table class="table personal_car_overview table-hover">
+
+        <tbody>
+            @foreach ($cars as $car)
                 <tr class="table-striped">
                     <th class="table_item car_picture">
-                        @if ($car->image == NULL)
-                            <img src="{{URL::asset('/images/placeholder-small.jpg')}}" alt="profile Pic" height="85" width="100">   
+                        @if ($car->image == null)
+                            <img src="{{ URL::asset('/images/placeholder-small.jpg') }}" alt="profile Pic" height="120"
+                                width="135">
                         @else
-                            {{$car->image}}
+                            {{ $car->image }}
                         @endif
                     </th>
                     <td class="table_item">
-                        <div class="kenteken license_plate_in_list">
+                        <div class="kenteken license_plate_in_list d-flex flex-column align-items-center">
                             <div class="inset">
-                            <div class="blue"></div>
-                            <input type="text" name="license_plate" disabled="" value="{{$car->license_plate}}" required=""/> 
+                                <div class="blue"></div>
+                                <input type="text" name="license_plate" disabled="" value="{{ $car->license_plate }}"
+                                    required="" />
+                            </div>
+                            <div class="sale-status">
+                                @isset($car->sold_at)
+                                    <p class="status_text border border-dark rounded text-light bg-danger">VERKOCHT</p>
+                                @endisset
+                                @empty($car->sold_at)
+                                    <p class="status_text border border-dark rounded text-light bg-success">TE KOOP</p>
+                                @endempty
                             </div>
                         </div>
                     </td>
-                    <td><div class="table-item">
-                        <p>{{$car->price}}</p>
-                    </div></td>
-                    <td><div class="table-item">
-                        <p>{{$car->make}} {{$car->model}}</p>
-                    </div></td>
-                    <td><div class="table-item">
-                        <form action="{{Route('car.destroy', $car->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn"><i class="bi bi-trash"></i></button>
-                        </form>    
-                    </div></td>
+                    <td>
+                        <div class="table-item">
+                            <p>{{ $car->price }}</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="table-item">
+                            <p>{{ $car->make }} {{ $car->model }}</p>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="table-item">
+                            <form action="{{ Route('car.destroy', $car->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn"><i class="bi bi-trash"></i></button>
+                            </form>
+                        </div>
+                    </td>
                 </tr>
-        @endforeach
+            @endforeach
 
-       
-    </tbody>
-</table>
+
+        </tbody>
+    </table>
 @endsection
